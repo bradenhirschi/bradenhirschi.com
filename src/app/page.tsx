f4ffcf185596ddd25b8086d430aba928dc5105fb
Braden Hirschi <bradenhirschi29@gmail.com>
2023-03-15 00:06:37 -0500

    Moved github section into its own component and temporarily hid it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,52 @@
 import Image from "next/image";
 import headshot from "../../public/headshot.jpg";
 
+const codeLinks: { link: string; title: string; description: string }[] = [
+  {
+    link: "https://github.com/bradenhirschi29/bradenhirschi.com",
+    title: "bradenhirschi.com",
+    description: "Open source code for this website!",
+  },
+];
+
+function Github() {
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen bg-zinc-200 dark:bg-zinc-800">
+      <div className="text-5xl uppercase pb-4">My GitHub Contributions</div>
+      <a href="https://github.com/bradenhirschi29">
+        <img
+          src="https://ghchart.rshah.org/3B82F6/bradenhirschi29"
+          alt="bradenhirschi29's GitHub contributions chart"
+        />
+      </a>
+
+      <div className="w-full my-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-10">
+          {codeLinks.map((codeLink) => (
+            <a
+              key={JSON.stringify(codeLink)}
+              href={codeLink.link}
+              target="_blank"
+              rel="noreferrer"
+              className="card hover:bg-zinc-50 dark:bg-zinc-50/5 dark:hover:bg-zinc-50/10"
+            >
+              <h1 className="label">{codeLink.title}</h1>
+              <h2 className="text-sm font-light secondary-label line-clamp-2">
+                {codeLink.description}
+              </h2>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default function Home() {
   return (
     <main>
       {/* Hero section */}
       <section className="min-h-screen px-8 md:px-16 lg:px-40 flex flex-col md:flex-row items-center justify-center lg:gap-16">
-
         {/* Left side w/ picture */}
         <div className="relative m-8">
           <Image
@@ -23,43 +63,38 @@ export default function Home() {
           </div>
           <div className="text-5xl uppercase pb-4 flex-col sm:flex-row">
             Braden
-            <span className="font-serif text-blue-600">
-              &nbsp;Hirschi
-            </span>
+            <span className="font-serif text-blue-600">&nbsp;Hirschi</span>
           </div>
           <div className="text-lg">
-            I&apos;m a passionate engineer and creative thinker working as a front-end web developer. I enjoy
-            building websites and web apps using modern tools like Angular, Node.js, and Express.js. As a software developer, I&apos;ve learned
-            the importance of working with a team, following industry standards/best practices for code
-            organization, and conducting thorough research to solve complex problems.
+            I&apos;m a passionate front-end engineer. I enjoy building websites
+            and web apps using modern tools like React, Node.js, and Express.js.
+            As a software developer, I&apos;ve learned the importance of working
+            with a team, following industry standards and best practices for
+            writing code, and conducting thorough research to solve complex
+            problems.
           </div>
           <div className="flex flex-row pt-6 gap-2 text-lg">
-            <a href="https://www.linkedin.com/in/braden-hirschi/"
-              className="py-2 px-4 border-2 border-blue-500 rounded-full">
+            <a
+              href="https://www.linkedin.com/in/braden-hirschi/"
+              className="py-2 px-4 border-2 border-blue-500 rounded-full"
+            >
               Connect on LinkedIn
             </a>
-            <a href="https://mobile.twitter.com/bradenhirschi29" className="py-2 px-4 border-2 border-zinc-700 dark:border-zinc-500 rounded-full">
+            <a
+              href="https://mobile.twitter.com/bradenhirschi29"
+              className="py-2 px-4 border-2 border-zinc-700 dark:border-zinc-500 rounded-full"
+            >
               Follow me on Twitter
             </a>
-            <a href="https://github.com/bradenhirschi29" className="py-2 px-4 border-2 border-zinc-500 dark:border-white rounded-full">
+            <a
+              href="https://github.com/bradenhirschi29"
+              className="py-2 px-4 border-2 border-zinc-500 dark:border-white rounded-full"
+            >
               View my GitHub
             </a>
           </div>
         </div>
-
       </section>
-
-      {/* GitHub contributions section */}
-      <section className="flex flex-col items-center justify-center min-h-screen bg-zinc-200 dark:bg-zinc-800">
-        <div className="text-5xl uppercase pb-4">
-          My GitHub Contributions
-        </div>
-
-        <a href="https://github.com/bradenhirschi29">
-          <img src="https://ghchart.rshah.org/3B82F6/bradenhirschi29" alt="bradenhirschi29's GitHub contributions chart" />
-        </a>
-      </section>
-
     </main>
-  )
+  );
 }
