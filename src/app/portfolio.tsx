274b70bd1c804bc32c43b041cd6bf7b69a74212f
Braden Hirschi <bradenhirschi29@gmail.com>
2023-07-25 00:56:20 -0500

    Added stock market game to portfolio

--- src/app/portfolio.tsx
+++ src/app/portfolio.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 import amyHirschiCoaching from "../../public/amy-hirschi-coaching.png";
+import stockmarketGame from "../../public/stockmarket-game.png";
 
 export default function Portfolio() {
   return (
     <section className="grid gap-8 bg-zinc-100 dark:bg-zinc-800 md:grid-cols-2 px-8 md:px-16 lg:px-40 py-24">
-      <h1>
-        My work
-      </h1>
+      <h1>My work</h1>
       <div></div>
-      <a className="card" href="https://amyhirschicoaching-com.vercel.app/?vercelToolbarCode=KDdCeG6f71YoHhZ">
-        <h5 className="mb-4">
-          Amy Hirschi Coaching
-        </h5>
-        <Image src={amyHirschiCoaching} alt={"Screenshot of amyhirschicoaching.com"} className="rounded-md"></Image>
-      </a>
-      <div className="card">
-        <h5>
-          More projects in development and coming soon!
-        </h5>
-      </div>
+      <Link className="card" href="https://amyhirschicoaching-com.vercel.app/">
+        <h5 className="mb-4">Amy Hirschi Coaching</h5>
+        <Image
+          src={amyHirschiCoaching}
+          alt={"Screenshot of amyhirschicoaching.com"}
+          className="rounded-md"
+        />
+      </Link>
+      <Link className="card" href="https://stockmarket-game-puce.vercel.app/">
+        <h5>(In progress) Clone of the 1963 Stock Market Game</h5>
+        <Image
+          src={stockmarketGame}
+          alt={"Screenshot of my Stock Market Game project"}
+          className="rounded-md"
+        />
+      </Link>
     </section>
-  )
+  );
 }
