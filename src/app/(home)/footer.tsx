4c89578caf17c4a787bcd7acb997827a36de87a4
Braden Hirschi <bradenhirschi29@gmail.com>
2023-07-26 01:08:28 -0500

    Created footer

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <section className="bg-background-secondary px-8 md:px-16 lg:px-40 py-12 text-center">
+      © 2023 Braden Hirschi
+    </section>
+  );
+};
+
+export default Footer;
