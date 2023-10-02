b00d597fb5dfdc53baed2b97156b6fcc5346ccf2
Braden Hirschi <bradenhirschi29@gmail.com>
2023-10-01 21:31:44 -0500

    Fixed colors for opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -23,15 +23,12 @@ export default async function OG() {
           style={{
             fontSize: "80px",
             fontWeight: "bold",
-            background:
-              "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
-            backgroundClip: "text",
-            color: "transparent",
+            color: "white",
             letterSpacing: "-0.02em",
           }}
         >
           Braden{" "}
-          <span style={{ color: "3B82F6", fontFamily: "serif" }}>Hirschi</span>
+          <span style={{ color: "#3B82F6", fontFamily: "serif" }}>Hirschi</span>
         </h1>
       </div>
     ),
