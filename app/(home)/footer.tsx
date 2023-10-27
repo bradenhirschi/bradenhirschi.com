aed9069d94adb199444abdab9ddf0e19db0de467
Braden Hirschi <bradenhirschi29@gmail.com>
2023-10-27 13:15:02 -0500

    Changed copyright to update every year

--- app/(home)/footer.tsx
+++ app/(home)/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <section className="bg-background-secondary px-4 md:px-16 lg:px-40 py-12 text-center">
-      © 2023 Braden Hirschi
+      <p>&copy; {new Date().getFullYear()} Braden Hirschi</p>
     </section>
   );
 };
