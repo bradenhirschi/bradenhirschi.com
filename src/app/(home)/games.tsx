05ae1bef5e5ee88485f95a97b527222df01603f1
Braden Hirschi <bradenhirschi29@gmail.com>
2025-05-23 09:44:35 -0500

    Added snap-start to games section

--- src/app/(home)/games.tsx
+++ src/app/(home)/games.tsx
@@ -2,7 +2,7 @@ const Games = () => {
   return (
     <section
       id="games"
-      className="flex items-center justify-center bg-background-secondary"
+      className="flex snap-start items-center justify-center bg-background-secondary"
     >
       <iframe src="https://silentspacemarine.com/" allowFullScreen></iframe>
     </section>
